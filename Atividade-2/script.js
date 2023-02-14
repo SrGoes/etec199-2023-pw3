3695ad8809e64576fb6b0765135fafff74e8ba0f
SrGoes <gabrielgaldino205@outlook.com>
2023-02-14 19:09:52 -0300

    git commit -m "Atividade 2"

--- Atividade-2/script.js
+++ Atividade-2/script.js
@@ -18,7 +18,7 @@ var Pessoas = [{
     "Nome": "Magno",
     "Idade": 80,
     "Altura": 1.83,
-    "Peso": 63,
+    "Peso": 150,
     "IMC": 0,
     "CategoriaIMC": "N/A"
 },
@@ -34,105 +34,59 @@ var Pessoas = [{
     "Nome": "Gabriel",
     "Idade": 18,
     "Altura": 1.74,
-    "Peso": 65,
+    "Peso": 50,
     "IMC": 0,
     "CategoriaIMC": "N/A"
-},
-{
-    "Nome": "Amanda",
-    "Idade": 17,
-    "Altura": 1.71,
-    "Peso": 66,
-    "IMC": 0,
-    "CategoriaIMC": "N/A"
-},
-{
-    "Nome": "Aline",
-    "Idade": 17,
-    "Altura": 1.62,
-    "Peso": 68,
-    "IMC": 0,
-    "CategoriaIMC": "N/A"
-},
-{
-    "Nome": "João",
-    "Idade": 80,
-    "Altura": 1.83,
-    "Peso": 63,
-    "IMC": 0,
-    "CategoriaIMC": "N/A"
-},
-{
-    "Nome": "Maria",
-    "Idade": 17,
-    "Altura": 1.63,
-    "Peso": 58,
-    "IMC": 0,
-    "CategoriaIMC": "N/A"
-},
-{
-    "Nome": "José",
-    "Idade": 18,
-    "Altura": 1.74,
-    "Peso": 65,
-    "IMC": 0,
-    "CategoriaIMC": "N/A"
-},
-
+}
 ];
 
 //função para calcular o IMC com o objeto Pessoas e editar o IMC de cada pessoa
 function calcularIMC() {
     for (var i = 0; i < Pessoas.length; i++) {
-        //Calculando o IMC de cada pessoa
         var pessoa = Pessoas[i];
-        pessoa.IMC = pessoa.Peso / (pessoa.Altura * pessoa.Altura);    
+        pessoa.IMC = pessoa.Peso / (pessoa.Altura * pessoa.Altura);
 
-        //Categorizando o IMC de cada pessoa
-        if (pessoa.IMC < 18.5) {
-            pessoa.CategoriaIMC = "Abaixo do peso!";
-        } else if (pessoa.IMC >= 18.5 && pessoa.IMC < 25) {
-            pessoa.CategoriaIMC = "Peso normal!";
-        } else if (pessoa.IMC >= 25 && pessoa.IMC < 30) {
-            pessoa.CategoriaIMC = "Sobrepeso!";
-        } else if (pessoa.IMC >= 30 && pessoa.IMC < 35) {
-            pessoa.CategoriaIMC = "Obesidade grau I !";
-        } else if (pessoa.IMC >= 35 && pessoa.IMC < 40) {
-            pessoa.CategoriaIMC = "Obesidade grau II !";
-        } else {
-            pessoa.CategoriaIMC = "Obesidade grau III !";
+        if(Pessoas[i].IMC <= 18.5){
+            Pessoas[i].CategoriaIMC = "Abaixo do peso!"
+        }else if(Pessoas[i].IMC <= 24.9){
+            Pessoas[i].CategoriaIMC = "Peso normal!"
+        }else if(Pessoas[i].IMC <= 29.9){
+            Pessoas[i].CategoriaIMC = "Sobrepeso!"
+        }else if(Pessoas[i].IMC <= 34.9){
+            Pessoas[i].CategoriaIMC = "Obesidade grau I !"
+        }else if(Pessoas[i].IMC <= 39.9){
+            Pessoas[i].CategoriaIMC = "Obesidade grau II !"
+        }else{
+            Pessoas[i].CategoriaIMC = "Obesidade grau III !"
         }
-    }
+}
 };
-
 calcularIMC();
+console.log(Pessoas);
 
-//função para mostrar o nome e o IMC de cada pessoa de acordo com o parâmetro passado
+//função para mostrar o nome e o IMC de cada pessoa 
 function mostrarIMC(Pessoa) {
         var texto = "";
         texto += Pessoa.Nome + " - " + Pessoa.IMC.toFixed(2) + " Categorizado(a): "+ Pessoa.CategoriaIMC +"<br>";
+
         document.getElementById("resultado").innerHTML = texto;
     };
 
-function separarPorCategoria(){
-    //Tabela com a quantidade de cada pessoa por categoria
-    var tabela = {
-        "Abaixo do peso!": 0,
-        "Peso normal!": 0,
-        "Sobrepeso!": 0,
-        "Obesidade grau I !": 0,
-        "Obesidade grau II !": 0,
-        "Obesidade grau III !": 0
-    };
-    //Contagem de cada pessoa por categoria
-    for (var i = 0; i < Pessoas.length; i++) {
-        var pessoa = Pessoas[i];
-        tabela[pessoa.CategoriaIMC] += 1;
+function exibirCategorias(){
+    var AbaixoP = 0;
+    var Normal = 0;
+    var Sobrepeso = 0;
+    var ObG1 = 0;
+    var ObG2 = 0;
+    var ObG3 = 0;
+
+    for(var i = 0; i < Pessoas.length; i++){
+        var Pessoa = Pessoas[i];
+        
+        if(Pessoa.CategoriaIMC = "Abaixo do peso!"){
+            AbaixoP++;
+        }else if(Pessoa.CategoriaIMC = "Peso normal!"){
+
+        }else if
     }
-    //Mostrar a tabela
-    var texto = "";
-    for (var categoria in tabela) {
-        texto += categoria + ": " + tabela[categoria] + "<br>";
-    }
-    document.getElementById("resultado").innerHTML = texto;
-};
+}
